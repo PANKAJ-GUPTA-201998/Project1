7d0b7e55b7a5adf63113822cd2fbd8e87f92b3ff
Pankaj Gupta <guptapankaj201998@gmail.com>
2024-02-23 20:14:30 +0530

    Your commit message

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+const usee=require('../controllers/users_controller')
+
+console.log('router loaded');
+router.get('/', homeController.home);
+router.use('/users', usee.profile);
+
+// for anty further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
+
+module.exports = router;
