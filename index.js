e2ddcca46b9d5cde3a084731f35edb06d7edd881
Pankaj Gupta <guptapankaj201998@gmail.com>
2024-02-27 17:29:00 +0530

    add css and js dynamically

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 const app = express();
 const port = 8000;
+app.use(express.static('./assets'));
 app.use(expressLayouts)
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
 app.set('view engine','ejs')
 app.set('views','./views')
 // use express router
 app.use('/', require('./routes'));
 //setup view engine
-
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
