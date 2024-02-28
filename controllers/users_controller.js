b85105a74e7f7a83110fd55603fa48f431523bcb
Pankaj Gupta <guptapankaj201998@gmail.com>
2024-02-28 15:01:25 +0530

    singup

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,9 @@
 module.exports.profile = function(req, res){
     return res.render('user_profile')
 }
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in')
+}
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up')
+}
