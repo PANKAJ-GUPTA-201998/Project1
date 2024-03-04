db30cb79d5739af5936d4ebc49cf011409741218
Pankaj Gupta <guptapankaj201998@gmail.com>
2024-03-04 18:13:20 +0530

    sign-in and sing-out created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,24 @@
 const { Session } = require("inspector")
 const User = require('../models/user')
 
-module.exports.profile = function(req, res){
-    return res.render('user_profile')
-}
+module.exports.profile = async function(req, res) {
+    try {
+        if (req.cookies.user_id) {
+            const user = await User.findById(req.cookies.user_id);
+            if (user) {
+                return res.render('user_profile');
+            }
+            return res.redirect('/sign-in');
+        } else {
+            return res.redirect('/sign-in');
+        }
+    } catch (err) {
+        console.error("Error finding user:", err);
+        return res.status(500).send("Internal Server Error");
+    }
+};
+
+
 module.exports.signIn=function(req,res){
     return res.render('user_sign_in')
 }
@@ -38,6 +53,31 @@ module.exports.create=function(req,res){
 
 
 
-module.exports.create_session=function(req,res){
-    //
+module.exports.create_session = async function(req, res) {
+    try {
+        // Find the user
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            console.log("User email not found");
+            return res.redirect('back');
+        }
+
+        // Handle incorrect password
+        if (user.password !== req.body.password) {
+            return res.redirect('back');
+        }
+
+        // Set user_id cookie and redirect to profile
+        res.cookie('user_id', user.id);
+        return res.redirect('/users/profile');
+    } catch (error) {
+        console.error("Error occurred:", error);
+        return res.redirect('back');
+    }
+}
+//to delete session by sign_out
+module.exports.delete_session=function(req,res){
+    res.clearCookie('user_id'); // Replace 'user_id' with the name of your cooki
+    res.redirect('/users/sign-in'); // Redirect to the desired location after deleting cokie
 }
--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const usersConrtoller = require('../controllers/users_controller');
 
-router.get('/profile', usersConrtoller.profile);
+router.get('/profile',usersConrtoller.profile);
 router.get('/sign-in',usersConrtoller.signIn);
 router.get('/sign-up',usersConrtoller.signUp);
 router.post('/create',usersConrtoller.create);
-router.get('/create-session',usersConrtoller.create_session);
+router.post('/create-session',usersConrtoller.create_session);
+router.get('/delete-session',usersConrtoller.delete_session);
 module.exports = router;
