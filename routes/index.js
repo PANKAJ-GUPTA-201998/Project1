621db25a6c5bd287e6a8d1444a24d1387aa99d20
Pankaj Gupta <guptapankaj201998@gmail.com>
2024-03-20 15:06:26 +0530

    post

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 express = require('express');
 
 const router = express.Router();
-const homeController = require('../controllers/home_controller');
 
-router.get('/', homeController.home);
+const postsController = require('../controllers/posts_controller');
 
-router.use('/users',require('./users'))
+router.get('/', postsController.getHomePage);
+
+router.use('/posts',require('./posts'));
+router.use('/users',require('./users'));
+
+
 // for anty further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
